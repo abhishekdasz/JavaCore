a30711fca6e86ce6c58feecabc7c09097b6a8d85
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-14 00:29:45 +0530

    pattern9

---
+++ 13Star/Pattern8.java
@@ -0,0 +1,19 @@
+public class Pattern8 
+{
+    public static void main(String[] args) 
+    {
+        int n = 10; 
+        for(int row=1; row<=n; row++)
+        {
+            for(int col=1; col<=row-1; col++)
+            {
+                System.out.print(" ");
+            }
+            for(int star=1; star<=((n-row)*2)+1; star++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 13Star/Pattern9.java
@@ -0,0 +1,29 @@
+public class Pattern9 
+{
+    public static void main(String[] args) 
+    {
+        int n = 4;
+        for(int row=1; row<=n; row++)
+        {
+            for(int col=1; col<=6; col++)
+            {
+                if(row==1 || row==n)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    if(col==1 || col==6)
+                    {
+                        System.out.print("* ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}

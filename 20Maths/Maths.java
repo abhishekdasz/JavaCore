8861377ef4f7672c1eeaf7b100596b404c85191a
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-20 23:50:14 +0530

    isPalindrome

--- 20Maths/Maths.java
+++ 20Maths/Maths.java
@@ -38,6 +38,31 @@ public class Maths
         }
         System.out.println(revnum);
     }
+
+    static boolean isPalindrome(int num)
+    {
+        int tempnum = num;
+        int rem;
+        int revnum = 0;
+        while(num != 0)
+        {
+            rem = num%10;
+            revnum = revnum*10;
+            revnum = revnum+rem;
+            num = num/10;
+        }
+        
+        System.out.println(tempnum);
+        System.out.println(revnum);
+        if(tempnum==revnum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     public static void main(String[] args) 
     {
         System.out.println("printDigits");
@@ -46,5 +71,8 @@ public class Maths
         countDigits(1932);
         System.out.println("reverse number");
         reverseNums(53127);
+        System.out.println("isPalindrome");
+        System.out.println(isPalindrome(12121));
+        System.out.println(isPalindrome(123));
     }
 }

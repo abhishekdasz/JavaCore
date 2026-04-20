6b2582ee969448f2064471d93404a7b0575e6af9
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-20 22:54:00 +0530

    countDigits

--- 20Maths/Maths.java
+++ 20Maths/Maths.java
@@ -10,10 +10,22 @@ public class Maths
             num = num/10;
         }
     }
-
+    static void countDigits(int num)
+    {
+        int rem;
+        int count=0;
+        while(num != 0)
+        {
+            rem = num%10;
+            System.out.println(rem);
+            num = num/10;
+            count++;
+        }
+        System.out.println("count"+count);
+    }
     public static void main(String[] args) 
     {
-        printDigits(53127);
-
+        // printDigits(53127);
+        countDigits(1234541237);
     }
 }

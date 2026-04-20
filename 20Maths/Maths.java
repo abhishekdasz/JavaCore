d0b5532e983fb95e2e9125ab5f5da085eb58d765
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-20 23:24:05 +0530

    reverseNum

--- 20Maths/Maths.java
+++ 20Maths/Maths.java
@@ -23,9 +23,28 @@ public class Maths
         }
         System.out.println("count"+count);
     }
+    static void reverseNums(int num) 
+    {
+        int rem;
+        int revnum = 0;
+        while(num != 0)
+        {
+            rem = num%10;
+            revnum = revnum*10;
+            revnum = revnum+rem;
+            
+            System.out.println(rem);
+            num = num/10;
+        }
+        System.out.println(revnum);
+    }
     public static void main(String[] args) 
     {
-        // printDigits(53127);
-        countDigits(1234541237);
+        System.out.println("printDigits");
+        printDigits(53127);
+        System.out.println("countDigits");
+        countDigits(1932);
+        System.out.println("reverse number");
+        reverseNums(53127);
     }
 }

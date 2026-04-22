965a2af232cef20b21fa3facbe2dc1f731d46479
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-22 23:31:20 +0530

    class and object

---
+++ 22Oops/Oops.java
@@ -0,0 +1,10 @@
+public class Oops 
+{
+    public static void main(String[] args) 
+    {
+        Student s = new Student();
+        s.name = "Abhishek";
+        s.study();
+        s.play();
+    }
+}
---
+++ 22Oops/Student.java
@@ -0,0 +1,16 @@
+public class Student 
+{
+    public int id;
+    public String name;
+    public int age; 
+    public int noSubjects;
+
+    public void study()
+    {
+        System.out.println(name+" is studying.");
+    }
+    public void play()
+    {
+        System.out.println(name+" is playing.");
+    }
+}

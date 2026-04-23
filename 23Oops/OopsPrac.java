9c573fd41d8bbd5a1cf982ea3054ffd4bde0950d
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-23 22:28:21 +0530

    constructor

--- 23Oops/OopsPrac.java
+++ 23Oops/OopsPrac.java
@@ -10,6 +10,12 @@ class Employee
     }
 }
 
+class Student
+{
+    int stud_id;
+    String stud_name;
+}
+
 public class OopsPrac {
     public static void main(String[] args) {
         System.out.println("hello world");
@@ -17,5 +23,16 @@ public class OopsPrac {
         Employee e = new Employee("Abhishek", 21);
         System.out.println(e.emp_name);
         System.out.println(e.emp_id);
+
+
+        Student s = new Student();
+        System.out.println(s.stud_id);
+        System.out.println(s.stud_name);
+        
+        s.stud_id = 10;
+        s.stud_name = "Das";
+
+        System.out.println(s.stud_id);
+        System.out.println(s.stud_name);
     }
 }

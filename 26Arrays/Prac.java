3935cf9a576c49566817a51ae89c3f6851c3be42
Abhishek Das <abhishekdaszy@gmail.com>
2026-04-26 18:22:07 +0530

    array practice

---
+++ 26Arrays/Prac.java
@@ -0,0 +1,16 @@
+class Prac
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = {10,20,30,40,50};
+        for(int num: arr)
+        {
+            System.out.println(num);
+        }
+
+        for(int i=0; i<=arr.length-1; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
